7ff58aa496486f80804bcad79eabda8cf11cf138
Patrick Bauer <patrick1bauer@gmail.com>
2023-01-18 23:04:39 -0500

    Initial commit!

---
+++ public/index.js
@@ -0,0 +1,24 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from 'firebase/app';
+// import { getAnalytics } from "firebase/analytics";
+// import { getAuth } from "firebase/auth";
+// import { getFirestore } from "firebase/firestore";
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAu7mDdg8SoY5DXQwcks5Q_le_aw2bEJ48",
+//     authDomain: "spotlight-trivia-app.firebaseapp.com",
+//     projectId: "spotlight-trivia-app",
+//     storageBucket: "spotlight-trivia-app.appspot.com",
+//     messagingSenderId: "575723277238",
+//     appId: "1:575723277238:web:ede0ad04f2c9f11d484f17",
+//     measurementId: "G-7J30SV35EL"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// const auth = getAuth(app);
+// const db = getFirestore(app);
+
+console.log('hello from index.js')
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Tools needed:
+node.js: https://nodejs.org/en/download/
+firebase: npm install firebase
+
+# How to build
+from project root: `npm run build`
+create local server: `firebase serve`
