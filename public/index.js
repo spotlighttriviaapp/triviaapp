f8e619d76836d3c6e51f17ab091838dc622c0b04
Patrick Bauer <patrick1bauer@gmail.com>
2023-01-22 14:27:49 -0500

    Added firebase imports & created basic ingest function that adds a single question

--- public/index.js
+++ public/index.js
@@ -1,24 +1,36 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from 'firebase/app';
-// import { getAnalytics } from "firebase/analytics";
-// import { getAuth } from "firebase/auth";
-// import { getFirestore } from "firebase/firestore";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore, collection, getDocs, doc, setDoc } from "firebase/firestore";
 
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//     apiKey: "AIzaSyAu7mDdg8SoY5DXQwcks5Q_le_aw2bEJ48",
-//     authDomain: "spotlight-trivia-app.firebaseapp.com",
-//     projectId: "spotlight-trivia-app",
-//     storageBucket: "spotlight-trivia-app.appspot.com",
-//     messagingSenderId: "575723277238",
-//     appId: "1:575723277238:web:ede0ad04f2c9f11d484f17",
-//     measurementId: "G-7J30SV35EL"
-// };
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAu7mDdg8SoY5DXQwcks5Q_le_aw2bEJ48",
+    authDomain: "spotlight-trivia-app.firebaseapp.com",
+    projectId: "spotlight-trivia-app",
+    storageBucket: "spotlight-trivia-app.appspot.com",
+    messagingSenderId: "575723277238",
+    appId: "1:575723277238:web:ede0ad04f2c9f11d484f17",
+    measurementId: "G-7J30SV35EL"
+};
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-// const auth = getAuth(app);
-// const db = getFirestore(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-console.log('Hello World! (index.js)')
+console.log('Hello World! (index.js)');
+
+
+myDocument = doc(db, "questions");
+
+myDocumentBody = {
+    question: "What is 1 + 1?",
+    answer: "2"
+};
+
+function ingest() {
+    setDoc(myDocument, myDocumentBody);
+};
