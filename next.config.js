ff54db7fcbae4cef9375e0e010553ba39dda43a6
Evan C. Navarro <EvanCNavarro@gmail.com>
2023-01-22 22:52:46 -0500

    module.exports

--- next.config.js
+++ next.config.js
@@ -3,4 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  webpack: (config) => {
+    config.experiments = config.experiments || {}
+    config.experiments.topLevelAwait = true
+    return config
+  },
+}
