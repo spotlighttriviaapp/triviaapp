6b39da14d5221e3646e556f86a98bbddd4699f63
Evan C. Navarro <EvanCNavarro@gmail.com>
2023-01-22 19:51:45 -0500

    testing 1

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className={styles.main}>
+      <div>Test Info</div>
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
