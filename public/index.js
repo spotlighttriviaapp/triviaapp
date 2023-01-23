f35079578551df6fe4f99ed3376df8705e26788f
Patrick Bauer <patrick1bauer@gmail.com>
2023-01-22 22:42:02 -0500

    New way to add documents to collection

--- public/index.js
+++ public/index.js
@@ -25,36 +25,41 @@ const db = getFirestore(app);
 
 console.log('Hello World! (index.js)');
 
-var myDocument = doc(db, "questions", "q1");
+// var myDocument = doc(db, "questions", "q1");
 
-var myDocumentBody = {
+// var myDocumentBody = {
+//     question: "What is 1 + 1?",
+//     answer: "2"
+// };
+
+// var myButtons = document.getElementsByClassName("btn");
+
+// function ingest() {
+//     // Convert csv into json
+//     const myCSV = fs.readFileSync("./triviaQuestionsTest.csv");
+//     var count = 0;
+//     Papa.parse(myCSV, {
+//         worker: true,
+//         step: function(result) {
+//             // do stuff with the result
+//             console.log(result);
+//         },
+//         complete: function(results, file) {
+//             console.log('parsing complete read', count, 'records.');
+//         }
+//     });
+
+//     setDoc(myDocument, myDocumentBody);
+//     console.log('Added document!');
+// };
+
+// for (var i = 0; i < myButtons.length; i++) {
+//     myButtons[i].addEventListener('click', ingest, false);
+// }
+
+const docRef = await addDoc(collection(db, "questions"), {
     question: "What is 1 + 1?",
     answer: "2"
-};
+});
 
-var myButtons = document.getElementsByClassName("btn");
-
-function ingest() {
-    // Convert csv into json
-    // const myCSV = fs.readFileSync("./triviaQuestionsTest.csv");
-    // var count = 0;
-    // Papa.parse(myCSV, {
-    //     worker: true,
-    //     step: function(result) {
-    //         // do stuff with the result
-    //         console.log(result);
-    //     },
-    //     complete: function(results, file) {
-    //         console.log('parsing complete read', count, 'records.');
-    //     }
-    // });
-
-    setDoc(myDocument, myDocumentBody);
-    console.log('Added document!');
-};
-
-
-
-for (var i = 0; i < myButtons.length; i++) {
-    myButtons[i].addEventListener('click', ingest, false);
-}
+console.log("Document written with ID: ", docRef.id);
