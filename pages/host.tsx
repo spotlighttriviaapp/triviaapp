099200d0c9999364d083ac99e436451b6ab2fb9c
Evan C. Navarro <EvanCNavarro@gmail.com>
2023-01-30 19:54:28 -0500

    dev push for demo to Denise
    added pages for all 5 interfaces

--- pages/host.tsx
+++ pages/host.tsx
@@ -1,18 +1,19 @@
 import Link from 'next/link'
 import PageWrapper from './pageWrapper'
 
-export default function Home() {
+export default function Host() {
   return (
     <PageWrapper>
-      <div className="container mx-auto">
+      <div className="container mx-auto bg-slate-200">
         <h1 className="pb-8 text-3xl font-bold">-----</h1>
-        Go to → <text className="pl-6 font-bold">Home</text>
+        Go to →
+        <Link href="/" className="pl-8 underline">
+          Home
+        </Link>
         <Link href="/admin" className="pl-6 underline">
           Admin
         </Link>
-        <Link href="/host" className="pl-6 underline">
-          Host
-        </Link>
+        <text className="pl-6 font-bold">Host</text>
         <Link href="/viewer" className="pl-6 underline">
           Viewer
         </Link>
